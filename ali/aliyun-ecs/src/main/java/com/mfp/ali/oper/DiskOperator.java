23c13bfa8785dabd4d9486250a2d1b552ff37f54
wanghengbin <wanghengbin@microfunplus.com>
2016-04-15 14:30:14 +0800

    init

---
+++ ali/aliyun-ecs/src/main/java/com/mfp/ali/oper/DiskOperator.java
@@ -0,0 +1,54 @@
+package com.mfp.ali.oper;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import com.aliyun.api.domain.Disk;
+import com.aliyun.api.ecs.ecs20140526.request.DescribeDisksRequest;
+import com.aliyun.api.ecs.ecs20140526.response.DescribeDisksResponse;
+import com.mfp.ali.util.ClientAdapter;
+import com.mfp.ali.util.Formatter;
+import com.taobao.api.ApiException;
+
+public class DiskOperator {
+    
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    private ClientAdapter client;
+    
+    public DiskOperator(ClientAdapter client) {
+        super();
+        this.client = client;
+    }
+
+    public List<Disk> getInstanceDataDisks(String regionId, String instanceId) throws ApiException{
+        DescribeDisksRequest request = new DescribeDisksRequest();
+        request.setRegionId(regionId);
+        request.setInstanceId(instanceId);
+        request.setDiskType("data");
+        
+        DescribeDisksResponse response = client.execute(request);
+        if(response.isSuccess()){
+//            logger.info("Describe data disks: " + Formatter.beanToJsonString(response.getDisks()));
+            return response.getDisks();
+        }else{
+            throw new RuntimeException("Describe data disks: Failed!!! " + response.getErrorCode());
+        }
+    }
+    
+    public Disk getInstanceSystemDisk(String regionId, String instanceId) throws ApiException{
+        DescribeDisksRequest request = new DescribeDisksRequest();
+        request.setRegionId(regionId);
+        request.setInstanceId(instanceId);
+        request.setDiskType("system");
+        
+        DescribeDisksResponse response = client.execute(request);
+        if(response.isSuccess()){
+//            logger.info("Describe system disk: " + Formatter.beanToJsonString(response.getDisks()));
+            return response.getDisks().get(0);   //promise get 0
+        }else{
+            throw new RuntimeException("Describe system disk: Failed!!! " + response.getErrorCode());
+        }
+    }
+
+}
---
+++ ali/aliyun-ecs/src/main/java/com/mfp/ali/oper/VSwitchOperator.java
@@ -0,0 +1,111 @@
+package com.mfp.ali.oper;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import com.aliyun.api.domain.VSwitch;
+import com.aliyun.api.ecs.ecs20140526.request.CreateVSwitchRequest;
+import com.aliyun.api.ecs.ecs20140526.request.DeleteVSwitchRequest;
+import com.aliyun.api.ecs.ecs20140526.request.DescribeVSwitchesRequest;
+import com.aliyun.api.ecs.ecs20140526.response.CreateVSwitchResponse;
+import com.aliyun.api.ecs.ecs20140526.response.DeleteVSwitchResponse;
+import com.aliyun.api.ecs.ecs20140526.response.DescribeVSwitchesResponse;
+import com.mfp.ali.util.ClientAdapter;
+import com.mfp.ali.util.Formatter;
+import com.taobao.api.ApiException;
+
+public class VSwitchOperator {
+    
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    private ClientAdapter client;
+    
+    public VSwitchOperator(ClientAdapter client) {
+        super();
+        this.client = client;
+    }
+
+    public VSwitch create(VSwitch model) throws ApiException{
+        VSwitch vSwitch = getByName(model.getVpcId(), model.getvSwitchName());
+        if(vSwitch != null){
+            logger.warning("Create VSwitch: VSwitch already exists. " + Formatter.beanToJsonString(vSwitch));
+            return vSwitch;
+        }
+        
+        CreateVSwitchRequest request = new CreateVSwitchRequest();
+        request.setZoneId(model.getZoneId());
+        request.setCidrBlock(model.getCidrBlock());
+        request.setVpcId(model.getVpcId());
+        request.setvSwitchName(model.getvSwitchName());
+        
+        CreateVSwitchResponse response = client.execute(request);
+        if(response.isSuccess()){
+            vSwitch = getByName(model.getVpcId(), model.getvSwitchName());
+            while("Pending".equals(vSwitch.getStatus())){
+                logger.info("Create VSwitch: Waiting...... " + Formatter.beanToJsonString(vSwitch));
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+                vSwitch = getByName(model.getVpcId(), model.getvSwitchName());
+            }
+            logger.info("Create VSwitch: Success! " + Formatter.beanToJsonString(vSwitch));
+            return vSwitch;
+        }else{
+            throw new RuntimeException("Create VSwitch: Failed!!! " + response.getErrorCode());
+        }
+    }
+    
+    public void destroy(String vpcId, String vSwitchName) throws ApiException{
+        VSwitch vSwitch = getByName(vpcId, vSwitchName);
+        if(vSwitch == null){
+            logger.warning("Destroy VSwitch: VSwitch not found. " + vSwitchName);
+            return;
+        }
+        _destroy(vSwitch);
+    }
+    
+    public void destroy(String vSwitchId) throws ApiException{
+        VSwitch vSwitch = new VSwitch();
+        vSwitch.setvSwitchId(vSwitchId);
+        _destroy(vSwitch);
+    }
+    
+    private void _destroy(VSwitch vSwitch) throws ApiException{
+        DeleteVSwitchRequest request = new DeleteVSwitchRequest();
+        request.setvSwitchId(vSwitch.getvSwitchId());
+        
+        DeleteVSwitchResponse response = client.execute(request);
+        if(response.isSuccess()){
+            logger.info("Destroy VSwitch: Waiting...... " + Formatter.beanToJsonString(vSwitch));
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+            logger.info("Destroy VSwitch: Success! " + Formatter.beanToJsonString(vSwitch));
+        }else{
+            throw new RuntimeException("Destroy VSwitch: Failed!!! " + response.getErrorCode());
+        }
+    }
+
+    public VSwitch getByName(String vpcId, String vSwitchName) throws ApiException{
+        DescribeVSwitchesRequest request = new DescribeVSwitchesRequest();
+        request.setVpcId(vpcId);
+        request.setPageSize(Long.valueOf(50));
+        
+        DescribeVSwitchesResponse response = client.execute(request);
+        if(response.isSuccess()){
+            List<VSwitch> list = response.getvSwitches();
+            if(list != null){
+                for(VSwitch item : list){
+                    if(vSwitchName.equals(item.getvSwitchName())){
+                        return item;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
